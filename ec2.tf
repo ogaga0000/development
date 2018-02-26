7d867e1a47b224183ba6a389f9aed86302abe67a
ogaga0000 <35418183+ogaga0000@users.noreply.github.com>
2018-02-26 14:06:40 +0900

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web1" {
     ami           = "ami-f80e0596"
     instance_type = "t2.micro"
     disable_api_termination = "false"
-    key_name = "terraform-test"
+    key_name = "terraform-nnet"
     subnet_id = "subnet-64b86413"
     tags {
         Name = "terraform-test"
